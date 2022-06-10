53d34fe2cbe1287509c0b1f48a08e9d68cffa899
IBRA110 <94391913+IBRA110@users.noreply.github.com>
2022-06-10 15:24:40 +0300

    Delete src directory

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: "AIzaSyAKlWk5_cDzFpw5ce02CADaSQQv0RmQbtM", // вставляем apiKey из сайта сервера
-  fbDbUrl: 'https://mamedov-frontend-default-rtdb.firebaseio.com/'
-};
